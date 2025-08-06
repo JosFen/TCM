81c2966346c3a84278903db2356af97b9aec05ce
Josceline Feng <josceline.feng@gmail.com>
2025-08-06 08:37:21 -0700

    leave app page for public education websites

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,15 @@
 // import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold">Welcome to the TCM Frontend</h1>
-      <p className="mt-4 text-lg">This is the main page of the frontend application.</p>
-      <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-        Admin Login
-      </button>
+        <Link href="/auth">
+          <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Admin Login
+          </button>
+        </Link>
     </div>
   );
 }
